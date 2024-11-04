f54e068439c8b95a4e3a3cc7c63e42d1caddcca1
BerserkerValle <fsamayoav@miumg.edu.gt>
2024-11-03 16:59:03 -0800

    data

--- app/config/env.js
+++ app/config/env.js
@@ -1,7 +1,7 @@
 
 const env = {
     database : 'antigua_umg2024_4145_o12o',
-    username : 'antigua_umg2024_4145_npci_user',
+    username : 'antigua_umg2024_4145_o12o_user',
     password : 'FXNWBnKTAG89zlMtrmOySur6z2ZXyEgJ',
     host : 'dpg-csa29go8fa8c73cji4t0-a.oregon-postgres.render.com',
     dialect : 'postgres',
